af284ada9376e03ddbe0b85de73c5a4c2ddb9669
shanmugapriyan18 <shanmugapriyan.240080@cce.ritchennai.edu.in>
2025-07-30 17:15:43 +0530

    Task3.py

--- Task3.py
+++ Task3.py
@@ -1,1 +1,23 @@
+# PASSWORD GENERATOR
 
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+def main():
+    print("\n--- PASSWORD GENERATOR ---")
+    
+    # User Input
+    length = int(input("Enter the desired length of the password: "))
+    
+    # Generate Password
+    password = generate_password(length)
+    
+    # Display the Password
+    print("Generated Password:", password)
+
+main()
