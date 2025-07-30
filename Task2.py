90a76932ed7d79f5105f7c8525740badf8e20820
shanmugapriyan18 <shanmugapriyan.240080@cce.ritchennai.edu.in>
2025-07-30 17:14:26 +0530

    Task2.py

--- Task2.py
+++ Task2.py
@@ -1,1 +1,28 @@
-task2.py created
+# SIMPLE CALCULATOR
+
+def calculator():
+    print("\n--- CALCULATOR ---")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    print("Choose operation: +  -  *  /")
+    operation = input("Enter operation: ")
+
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            print("Error: Division by zero.")
+            return
+    else:
+        print("Invalid operation.")
+        return
+
+    print("Result:", result)
+
+calculator()
